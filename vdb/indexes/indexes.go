608cfcc780af3499cc8bd1dd749c706edecbebe4
renkui <371397938@qq.com>
2019-08-02 16:19:34 +0800

    Update indexes.go

--- vdb/indexes/indexes.go
+++ vdb/indexes/indexes.go
@@ -24,7 +24,7 @@ var LatestIndexKey = []byte("LATEST")
 var log = logging.MustGetLogger("IndexesServices")
 
 /// Deve
-const AIndexesKeyPathPrefix = "/aya/chain/indexes/dev/0802/10/"
+const AIndexesKeyPathPrefix = "/aya/chain/indexes/dev/0802/11/"
 /// Prod
 //const AIndexesKeyPathPrefix = "/aya/chain/indexes/"
 
